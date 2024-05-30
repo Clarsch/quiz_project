2b09ec54e35dcf8cab3d416093cb758080f26289
Chris <chris@localhost.localdomain>
2024-05-30 22:46:48 +0200

    Initial commit with code from Bari Hackathon

---
+++ backend/src/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"quizzy_game/api"
+	"quizzy_game/clientManagement"
+)
+
+func getFrontPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func main() {
+	http.HandleFunc("/", getFrontPage)
+	http.HandleFunc("/questions", api.GetQuestionsWeb)
+	http.HandleFunc("/categories", api.GetCategoriesWeb)
+	http.HandleFunc("/ws", clientManagement.WsEndpoint)
+	http.HandleFunc("/ws/", clientManagement.WsEndpoint)
+
+	http.ListenAndServe(":3333", nil)
+
+}
